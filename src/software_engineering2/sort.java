56cd3e010638439bc243d816ba1a7e98f033c5e1
MOHITBHAT <mbcse50@gmail.com>
2020-01-30 11:42:37 +0530

    Branch code

---
+++ src/software_engineering2/sort.java
@@ -0,0 +1,14 @@
+package software_engineering2;
+
+public class sort {
+
+	public static void main(String[] args) {
+		int arr[]= {1,4,3,7,6,5,99,88,76};
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
